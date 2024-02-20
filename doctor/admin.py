61286e63c8f0958bce2babe310285ff54028f798
RadwanHegazy <radwangaber22@gmail.com>
2024-02-20 10:18:35 +0200

    write updates on doctor app

--- doctor/admin.py
+++ doctor/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.models import  Group
-from .models import Doctor, Department
+from .models import Doctor, Department, DoctorModel
 
-admin.site.unregister([Group])
+admin.site.unregister(Group)
 
-admin.site.register([Doctor, Department ])
+
+
+
+class DocPanel (admin.ModelAdmin) : 
+    list_display = ('full_name','email','department',)
+
+admin.site.register(DoctorModel,DocPanel)
+admin.site.register(Department)
